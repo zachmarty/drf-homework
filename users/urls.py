c539d227eed8e233d9d111bd99a306048eb8c5fa
Pavel <pashapasha-2013@mail.ru>
2024-05-14 19:06:44 +0300

    custom tokenobtain

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserCreateView
+from users.views import CustomTokenObtainPairView, UserCreateView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("create", UserCreateView.as_view(), name="user_create"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 from django.contrib.auth.hashers import make_password
 from rest_framework.generics import CreateAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 from users.models import User
 from users.serializers import UserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class UserCreateView(CreateAPIView):
@@ -12,3 +16,12 @@ class UserCreateView(CreateAPIView):
         new_user = serializer.save()
         new_user.password = make_password(new_user.password)
         new_user.save()
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        response = super().post(request, *args, **kwargs)
+        user = User.objects.get(email=request.data["email"])
+        user.last_login = datetime.now()
+        user.save()
+        return response
